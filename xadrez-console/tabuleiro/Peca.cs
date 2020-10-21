4bf8bb2679d6505e147193e96bb8ad3b14326e46
Jackson Alves <jackson.alves.marcolino@gmail.com>
2020-10-21 01:35:53 -0300

    Começando a implementar um metodo que imprime o tabuleiro

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -1,10 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using tabuleiro;
 
 namespace xadrez_console.tabuleiro
 {
     class Peca
     {
+        public Posicao posicao { get; set; }
+        public Cor cor { get;protected set; }
+        public int QTeMovimentos { get; set; }
+        public Tabuleiro tabuleiro { get; set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        {
+            this.posicao = posicao;
+            this.cor = cor;
+            QTeMovimentos = 0;
+            this.tabuleiro = tabuleiro;
+        }
     }
 }
--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using xadrez_console.tabuleiro;
 
-namespace xadrez_console.tabuleiro
+namespace tabuleiro
 {
     class Tabuleiro
     {
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+        private Peca[,] pecas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            Linhas = linhas;
+            Colunas = colunas;
+            pecas = new Peca[linhas, colunas];
+        }
+
+        public Peca peca(int linha, int coluna)
+        {
+            return pecas[linha, coluna];
+        }
     }
 }
