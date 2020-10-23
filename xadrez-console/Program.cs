16a8f809b4546d12c158b380420d9ee9cc94372a
Jackson Alves <jackson.alves.marcolino@gmail.com>
2020-10-23 10:13:44 -0300

    Implementando a logica de xequemate

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -43,7 +43,10 @@ namespace xadrez_console
                         Console.WriteLine(e.Message);
                         Console.ReadLine();
                     }
+
                 }
+                Console.Clear();
+                Tela.imprimirPartida(partida);
 
             }
             catch (TabuleiroException e)
