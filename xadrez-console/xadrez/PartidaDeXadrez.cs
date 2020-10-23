6d67bfa5aeaec0d27efde111d0b87eae076891d8
Jackson Alves <jackson.alves.marcolino@gmail.com>
2020-10-23 10:17:55 -0300

    Mudando o nome do metodo para movimentoPossivel

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -98,7 +98,7 @@ namespace xadrez
 
         public void validarPosicaoDeDestino(Posicao origem, Posicao destino)
         {
-            if (!tab.peca(origem).podeMoverPara(destino))
+            if (!tab.peca(origem).movimentoPossivel(destino))
             {
                 throw new TabuleiroException("Posição de destino invalida!");
             }
@@ -203,10 +203,11 @@ namespace xadrez
                     {
                         if (mat[i, j])
                         {
+                            Posicao origem = x.posicao;
                             Posicao destino = new Posicao(i, j);
-                            Peca pecaCapturada = executaMovimento(x.posicao, destino);
+                            Peca pecaCapturada = executaMovimento(origem, destino);
                             bool testeXeque = estaEmXeque(cor);
-                            desFazMovimento(x.posicao, destino, pecaCapturada);
+                            desFazMovimento(origem, destino, pecaCapturada);
                             if (!testeXeque)
                             {
                                 return false;
